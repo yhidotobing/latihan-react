e34e81f5ccfe00a8f0a501e41e69dc4494522290
yhidotobing <yhidotobing@gmail.com>
2021-11-20 16:31:49 +0700

    Update

--- src/pages/LatihanHooks/index.js
+++ src/pages/LatihanHooks/index.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 
 function LatihanHooks() {
   const [nilai, setNilai] = useState(0);
-  const umur = 0;
 
   useEffect(() => {
     console.log(nilai); // ini akan di jalankan pertama di render dan ketika componentdiUpdate
